05566546ff28ce2b481a5b67bca87afb0224e216
Brian Yang <yangbri1@msu.edu>
2025-01-27 02:06:24 +0000

    Utilized Collections.sort() method on given 'num' List to first sort it in ascending fashion --- two biggest Integer's would be at the end. Next, retrieve the last two elements from the now sorted list & add them together to return the sum.

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,5 +1,9 @@
 
+// import 'List' class from Java's utility package to use 'List' collection & its respective built-in methods
 import java.util.List;
+// import 'Collections' -- super-parent class of 'List' to use .sort() method
+// https://stackoverflow.com/questions/16252269/how-to-sort-a-list-arraylist
+import java.util.Collections;
 
 public class LargestSum {
     /**
@@ -10,6 +14,15 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        // utilize built-in .sort() method from Collections super-parent class to sort 'nums' in "natural" order -- default: ASC
+        Collections.sort(nums);
+        // Collections.reverse(nums);   // enable this if want in DESC order
+        // retrieve .size() of 'nums' List
+        int numsSize = nums.size();
+        // .get() last element & 2nd to last element in now sorted 'nums' List in ascending fashion --- aka these are the 2 largest Integers
+        int lastElem = nums.get(numsSize - 1);
+        int secLastElem = nums.get(numsSize - 2);
+        // return sum of the two largest Integers
+        return (lastElem + secLastElem);
     }
 }
